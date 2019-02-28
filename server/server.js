e28b69dfae72b8c6e18c6ff269f4847a076b1683
lailaweil <lweil@est.frba.utn.edu.ar>
2019-02-28 15:13:54 -0300

    probando

--- server/server.js
+++ server/server.js
@@ -1,44 +1,38 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-<<<<<<< HEAD
 mongoose.connect('mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true
 });
 
 var Todo = mongoose.model('Todo', { //seteo de modelo para la database
     text: {
-=======
 mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
 var Todo = mongoose.model('Todo', { //seteo de modelo para la database
     text :{
->>>>>>> 44f4f882b5bab6880025ce942b42a82e4a847a46
         type: String
     },
     completed: {
         type: Boolean
     },
-<<<<<<< HEAD
     completedAt: {
-=======
     completedAt:{
->>>>>>> 44f4f882b5bab6880025ce942b42a82e4a847a46
         type: Number
     }
 });
 
-// var newTodo = new Todo({ //creo una instancia de Todo
-//     text: 'Cook dinner'
-// });
+var newTodo = new Todo({ //creo una instancia de Todo
+    text: 'Cook dinner'
+});
 
-// //guardo
-// newTodo.save().then((doc)=>{
-//     console.log('Tarea guardada con éxito.');
-//     console.log(doc);
-// }, (e)=>{
-//     console.log('No se pudo guardar la tarea.');
-// });
+//guardo
+newTodo.save().then((doc)=>{
+    console.log('Tarea guardada con éxito.');
+    console.log(doc);
+}, (e)=>{
+    console.log('No se pudo guardar la tarea.');
+});
 
 var otherTodo = new Todo({
     text: 'otro to do',
