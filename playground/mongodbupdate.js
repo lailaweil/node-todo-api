86f5346291b7bbf28ae262aa9423c720cd9ed7a4
lailaweil <lweil@est.frba.utn.edu.ar>
2019-03-04 16:24:21 -0300

    added PATCH /todos/:id

--- playground/mongodbupdate.js
+++ playground/mongodbupdate.js
@@ -1,7 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true }, (err, client)=>{ //te conectas al server
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true },{ useFindAndModify: false }, (err, client)=>{ //te conectas al server
     if(err){
         return console.log('Unabke to connect to database');
     }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyparser = require('body-parser');
+const express = require('express');
+const _ = require('lodash');
+const bodyparser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require ('./db/mongoose');
@@ -11,6 +12,8 @@ var app = express();
 const port = process.env.PORT || 3000
 
 app.use(bodyparser.json());
+
+mongoose.set('useFindAndModify', false);
 
 app.post('/todos',(req, res)=>{
     var todo = new Todo({
@@ -35,7 +38,9 @@ app.get('/todos',(req,res)=>{
 
 app.get('/todos/:id', (req,res)=>{
     var id = req.params.id;
+
     if(!ObjectID.isValid(id)) return res.status(404).send();
+
     Todo.findById(id).then((todo)=>{
         if(!todo) res.status(404).send();
         res.send(todo);
@@ -48,12 +53,36 @@ app.get('/todos/:id', (req,res)=>{
 
 app.delete('/todos/:id',(req,res)=>{
     var id = req.params.id;
+
     if(!ObjectID.isValid(id)) return res.status(404).send();
+
     Todo.findByIdAndDelete(id).then((todo)=>{
         if(!todo) res.status(404).send();
         res.send(todo);
         console.log(todo);
     }).catch((e)=>{
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text', 'completed']); //user no puede update el completedAt
+
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+ 
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+        if(!todo) res.status(404).send();
+        res.send({todo});
+    }).catch((err)=>{
         res.status(400).send();
     });
 });
